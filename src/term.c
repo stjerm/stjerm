6a3734371ce8613c379132f2d19b6ce308b0b6e7
Denis Ollier <larchunix@users.noreply.github.com>
2016-06-13 04:53:50 +0200

    Don't check for vte >= 0.25 in code as configure requires vte >= 0.28

--- src/term.c
+++ src/term.c
@@ -242,7 +242,6 @@ static void term_app_request(VteTerminal *term, gpointer user_data)
 
 static void term_fork_command(VteTerminal *term, char *cmd)
 {
-    #if VTE_CHECK_VERSION(0,25,0)
     char **argv = NULL;
     
     g_shell_parse_argv(cmd, NULL, &argv, NULL);
@@ -250,8 +249,5 @@ static void term_fork_command(VteTerminal *term, char *cmd)
     vte_terminal_fork_command_full(term, VTE_PTY_DEFAULT, "", argv, NULL, 
         G_SPAWN_CHILD_INHERITS_STDIN|G_SPAWN_SEARCH_PATH,
         NULL, NULL, NULL, NULL);
-    #else
-    vte_terminal_fork_command(term, cmd, NULL, NULL, "", TRUE, TRUE, TRUE);
-    #endif
 }
 
